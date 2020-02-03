7c4e45d3b5c33c3a263ccff1fd4ae58f50313f46
Leo Neto <lneto@sharpencx.com>
2020-02-03 01:55:31 -0500

    Update Slack notification message

--- notify.sh
+++ notify.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Notify to Slack
 
-curl -X POST -H 'Content-type: application/json' --data '{"text": "Radio Luzeiros is Live!" }' https://hooks.slack.com/services/$SLACK_TOKEN
+curl -X POST -H 'Content-type: application/json' --data '{"text": "Radio Luzeiros deployed successfully!" }' https://hooks.slack.com/services/$SLACK_TOKEN
