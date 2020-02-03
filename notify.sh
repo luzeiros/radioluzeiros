4d742104ae947838b035faf72103a5867aa9dc94
Leo Neto <lneto@sharpencx.com>
2020-02-03 01:09:59 -0500

    Update app.json and slack notify script

---
+++ notify.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Notify to Slack
+
+curl -X POST -H 'Content-type: application/json' --data '{"text": "Radio Luzeiros is Live!" }' https://hooks.slack.com/services/$SLACK_TOKEN
